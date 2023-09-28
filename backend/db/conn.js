31b474e8e67e32378683a3534e7bba15f9bc3335
anupam singh <anupamsingh2389@gmail.com>
2023-09-28 17:36:56 +0530

    add

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
+
+const dbPassword = process.env.DB_PASS;
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://anupamsingh2389:NxikFqGlHfuCV8sQ@cluster0.y4rmw8e.mongodb.net/registrationdata?retryWrites=true&w=majority';
+const mongoURI = `mongodb+srv://anupamsingh2389:${dbPassword}@cluster0.y4rmw8e.mongodb.net/registrationdata?retryWrites=true&w=majority`;
 // const mongoURI = 'mongodb://127.0.0.1:27017/registrationdata';
 
 mongoose.connect(mongoURI).then(() => {
